593bb774bdf9f9e8908b0287a730d99858b6cfe6
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-03-18 20:22:50 -0300

    chore: add jest scripts, unit and integration configs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverage: true,
+module.exports = {
+  roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
