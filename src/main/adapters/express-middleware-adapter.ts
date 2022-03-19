7ac04606e1ce651775120a2620ee9da0fafe3847
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-03-18 22:35:48 -0300

    feat: add express adapters

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { Middleware } from '@/presentation/protocols'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const request = {
+      ...(req.headers || {})
+    }
+    const httpResponse = await middleware.handle(request)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
