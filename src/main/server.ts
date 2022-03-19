9a75ec2bd34a1136f969bc1bc38af5fc671c3a7e
Stephano Balbinot <stephanobalbinot@hotmail.com>
2022-03-18 21:30:35 -0300

    chore: add supertest

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
 import 'module-alias/register'
-import app from '@/main/config/app'
+import app from './config/app'
 import env from '@/main/config/env'
 
 app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
